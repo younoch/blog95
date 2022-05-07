29c9a90a6301324ab1b6daa33f21a20cd1f24b8a
younoch-devxhub <git.younoch@devxhub.com>
2022-05-08 02:11:06 +0600

    remove base url

--- src/router/index.js
+++ src/router/index.js
@@ -124,7 +124,7 @@ const routes = [
 
 const router = new VueRouter({
   mode: "history",
-  base: process.env.BASE_URL,
+  // base: process.env.BASE_URL,
   routes,
   scrollBehavior() {
     return { x: 0, y: 0 };
