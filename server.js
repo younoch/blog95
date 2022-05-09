186536fe8aa8e3b15b5bc2727745561353a477c3
younoch-devxhub <git.younoch@devxhub.com>
2022-05-09 10:55:21 +0600

    Change start modudule

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const serveStatic = require('serve-static')
+const path = require('path')
+
+const app = express()
+
+app.use('/', serveStatic(path.join(__dirname, '/dist')))
+
+app.get(/.*/, function(req, res) {
+  res.sendFile(path.join(__dirname, '/dist/index.html'))
+})
+
+const port = process.env.PORT || 8000
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
